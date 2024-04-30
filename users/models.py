55ca2ee62d8763978f186ca1843933259966e8d4
prosper <edwardprosper001@gmail.com>
2024-04-30 08:01:21 +0100

    'updates'

--- users/models.py
+++ users/models.py
@@ -18,6 +18,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_lecturer = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
@@ -32,43 +33,15 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.username
     
 
-
-class Lecturer(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=200, unique=True)
-    title = models.CharField(max_length=100, choices=TITLE, default="Engr.")
-    email = models.EmailField(_("email address"), unique=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.email
-    
-    def get_full_name(self):
-        return self.username
     
 
 
-class LecturerProfile(models.Model):
-    user = models.OneToOneField(Lecturer, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="profile_pictures", default="lecturer.jpg")
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="profile_pictures", default="user.jpg")
     bio = models.TextField()
 
     def __str__(self):
         return f"{self.user.title} {self.user.username}"
     
-
-
-class StudentProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="profile_pictures", default="lecturer.jpg")
-    bio = models.TextField()
-
-    def __str__(self):
-        return f"{self.user.username}"
 
--- users/signals.py
+++ users/signals.py
@@ -6,27 +6,16 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 @receiver(post_save, sender=Lecturer)
-def create_lecturer_profile(sender, created, instance, **kwargs):
+def create_profile(sender, created, instance, **kwargs):
     if created:
-        LecturerProfile.objects.create(user=instance)
-
-
-
-@receiver(post_save, sender=User)
-def create_student_profile(sender, created, instance, **kwargs):
-    if created:
-        LecturerProfile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=Lecturer) 
-def save_lecturer_profile(sender, instance, **kwargs):
-        instance.profile.save()
+        User.objects.create(user=instance)
 
 
 
 @receiver(post_save, sender=User) 
-def save_student_profile(sender, instance, **kwargs):
+def save_lecturer_profile(sender, instance, **kwargs):
         instance.profile.save()
+
 
 
 
