65fa48435087110a87c5f9e6eecdd5639ed90393
prosper <edwardprosper001@gmail.com>
2024-04-30 08:07:10 +0100

    'updates'

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Profile
+
+admin.site.register(Profile)
 
 
 admin.site.register(User)
--- users/signals.py
+++ users/signals.py
@@ -1,11 +1,10 @@
 from django.db.models.signals import post_save
-from .models import Lecturer, LecturerProfile, StudentProfile
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-@receiver(post_save, sender=Lecturer)
+@receiver(post_save, sender=User)
 def create_profile(sender, created, instance, **kwargs):
     if created:
         User.objects.create(user=instance)
