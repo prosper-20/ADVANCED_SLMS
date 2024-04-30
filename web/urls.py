2d13c508de2cbc9e1235c0cb2e79a24f3b2b0e42
prosper <edwardprosper001@gmail.com>
2024-04-30 08:10:48 +0100

    'updates'

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePage, CoursesPage, AboutUsPage, CourseDetailPage, LoginPage, SignUpPage
+from .views import HomePage, CoursesPage, AboutUsPage, CourseDetailPage, LoginPage, SignUpPage, StaffLoginPage, StaffSignUpPage
 
 urlpatterns = [
     path("", HomePage.as_view(), name="web-home"),
@@ -7,5 +7,7 @@ urlpatterns = [
     path("courses/", CoursesPage.as_view(), name="courses"),
     path("courses/<slug:slug>/", CourseDetailPage.as_view(), name="course-detail"),
     path("login/", LoginPage.as_view(), name="web-login"),
-    path("signup/", SignUpPage.as_view(), name="web-signup")
+    path("staff/login/", StaffLoginPage.as_view(), name="web-staff-login"),
+    path("signup/", SignUpPage.as_view(), name="web-signup"),
+    path("staff/signup/", StaffSignUpPage.as_view(), name="web-staff-signup")
 ]
--- web/views.py
+++ web/views.py
@@ -9,6 +9,23 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class StaffLoginPage(View):
+    def get(self, request):
+        return render(request, "web/authentication/login.html")
+    
+    def post(self, request):
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect(reverse('web-home'))
+        else:
+            messages.info(request, "Invalid username/password")
+            return redirect(reverse('web-login'))
+
 class LoginPage(View):
     def get(self, request):
         return render(request, "web/authentication/login.html")
@@ -28,6 +45,31 @@ class LoginPage(View):
 
 
 class SignUpPage(View):
+    def get(self, request):
+        return render(request, "web/authentication/signup.html")
+    
+    def post(self, request):
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email already exists")
+                return redirect(reverse('web-signup'))
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Username already exists")
+                return redirect(reverse('web-signup'))
+            else:
+                User.objects.create_user(username=username, email=email, password=password)
+                messages.info(request, "Account creation successful")
+                return redirect(reverse('web-login'))
+        else:
+            messages.info(request, "Passwords do not match")
+            return redirect(reverse('web-signup'))
+        
+class StaffSignUpPage(View):
     def get(self, request):
         return render(request, "web/authentication/signup.html")
     
