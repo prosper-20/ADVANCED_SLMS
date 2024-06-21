0f3c9a020c37ffb52c829d81adfed04f8b47bc9c
prosper <edwardprosper001@gmail.com>
2024-06-20 23:51:55 -0500

    'updates'

--- web/admin.py
+++ web/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from .models import Post, Newsletter
+from .models import Post, Newsletter, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    prepopulated_fields = {"slug": ("name",)}
+admin.site.register(Category, CategoryAdmin)
 
 
 admin.site.register(Newsletter)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author']
+    list_display = ['title', 'author', 'category']
     list_filter = ['title']
-    list_editable = ['author']
+    list_editable = ['author', 'category']
     raw_id_fields =['author']
     prepopulated_fields = {"slug": ("title",)}
 
--- web/models.py
+++ web/models.py
@@ -7,6 +7,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
